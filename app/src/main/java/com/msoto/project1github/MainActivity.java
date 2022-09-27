a0eae706f2fcbadc541b0037a858a1ef8cfe1b32
marcelo <marcelos070r@gmail.com>
2022-09-27 10:27:58 -0300

    Initial commit

---
+++ app/src/main/java/com/msoto/project1github/MainActivity.java
@@ -0,0 +1,17 @@
+package com.msoto.project1github;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        
+
+    }
+}
